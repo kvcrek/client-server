788ac1679406201e3db9c2c9f6224ef9a147a92f
qocureq <makskocur@gmail.com>
2023-03-29 00:59:58 +0200

    update

--- boost_asio_tcp/server.cpp
+++ boost_asio_tcp/server.cpp
@@ -1,43 +1,84 @@
 #include <iostream>
 #include <boost/asio.hpp>
-#include <thread>
+
 
 #define SERVER_IP "127.0.0.1"
 #define SERVER_PORT 8080
 #define MAX_BUFFER_LEN 4096
 
-using namespace boost;
+using boost::asio::ip::tcp;
 
-void handleConnection(asio::ip::tcp::socket sock) {
-    try {
+class Connection : public std::enable_shared_from_this<Connection> {
+public:
+    explicit Connection(tcp::socket socket)
+            : sock(std::move(socket)),
+              buf{} {
         std::cout << "New connection from: " << sock.remote_endpoint().address().to_string() << std::endl;
-        while (true) {
-            char buf[MAX_BUFFER_LEN]{};
-            system::error_code err;
+    }
 
-            sock.read_some(asio::buffer(buf), err);
-            if (err == asio::error::eof) {
-                std::cout << sock.remote_endpoint().address().to_string() << " has disconnected." << std::endl;
-                return;
-            } else if (err) {
-                throw system::system_error(err);
-            }
-            std::cout << "Message from client : " << buf << std::endl;
-            asio::write(sock, asio::buffer("Hello from server!"));
-        }
+    void start() {
+        do_read();
     }
-    catch (std::exception &e) {
-        std::cerr << "Exception in thread : " << e.what() << std::endl;
+
+    void do_read() {
+        auto self(shared_from_this());
+        sock.async_read_some(
+                boost::asio::buffer(buf),
+                [this, self](boost::system::error_code ec, std::size_t bytes_transferred) {
+                    if (!ec) {
+                        do_write();
+                    }
+                    std::cout << "Message from client" << buf.data() << std::endl;
+                });
     }
+
+    void do_write() {
+        auto self(shared_from_this());
+        boost::asio::async_write(sock,
+                                 boost::asio::buffer("Hello from server!"),
+                                 [this, self](boost::system::error_code err, std::size_t len) -> void {
+                                     if (!err) {
+                                         sock.shutdown(tcp::socket::shutdown_send);
+                                         std::cout << sock.remote_endpoint().address().to_string()
+                                                   << " has disconnected." << std::endl;
+                                     }
+                                 });
+    }
+
+private:
+    tcp::socket sock;
+    std::array<char, MAX_BUFFER_LEN> buf;
+};
+
+class Server {
+public:
+    explicit Server(boost::asio::io_service &io_service)
+            : acceptor_(io_service, tcp::endpoint(boost::asio::ip::address::from_string(SERVER_IP), SERVER_PORT)) {
+        do_accept();
+    }
+
+    void do_accept() {
+        acceptor_.async_accept(
+                [this](boost::system::error_code ec, tcp::socket socket) {
+                    if (!ec) {
+                        std::make_shared<Connection>(std::move(socket))->start();
+                    }
+                    do_accept();
+                });
+    }
+
+private:
+    tcp::acceptor acceptor_;
 };
 
 int main() {
-    asio::io_context ioContext;
-
-    asio::ip::tcp::acceptor acc(ioContext,
-                                asio::ip::tcp::endpoint(asio::ip::address::from_string(SERVER_IP), SERVER_PORT));
-    while (true) {
-        std::thread(handleConnection, acc.accept()).detach();
+    try {
+        boost::asio::io_service ios;
+        Server server(ios);
+        ios.run();
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
     }
+
     return 0;
 }
